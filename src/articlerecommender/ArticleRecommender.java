2d1fea7c19765adb2b662e974cbbc44336536b13
Michael Soprano <miccighel@gmail.com>
2014-12-04 17:24:25 +0100

    Cose varie

--- src/articlerecommender/ArticleRecommender.java
+++ src/articlerecommender/ArticleRecommender.java
@@ -1,34 +1,48 @@
 package articlerecommender;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List; 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory; 
-import org.xml.sax.SAXException;
+import de.l3s.boilerpipe.extractors.ArticleExtractor;
+import de.l3s.boilerpipe.extractors.DefaultExtractor;
+import java.net.URL;
   
 class ArticleRecommender {
  
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         
-        SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
-        
-        try {
-            
-            SAXParser saxParser = saxParserFactory.newSAXParser();
-            Parsing handler = new Parsing();
-            saxParser.parse(new File("data/test.xml"), handler);
-            
-            List<Articolo> articoli = handler.getArticoli();
-            
-            for(Articolo articolo : articoli){
-                System.out.println(articolo.getAnno());
-            }
-        
-        } catch (ParserConfigurationException | SAXException | IOException e) {
-            e.getMessage();
+        URL url = new URL("http://link.springer.com/article/10.1007/s10639-014-9371-3");
+        String text = DefaultExtractor.INSTANCE.getText(url);
+        String[] split = text.split("\n");
+        int i = 0;
+        while(!split[i].equals("Abstract")){
+            i++;
         }
+        
+        String text2 = ArticleExtractor.INSTANCE.getText(url);
+        String[] split2 = text2.split("\n");
+        
+        i = 0;
+        while(!split2[i].contains("References")){
+            i++;
+        }
+
+        String cit = split2[4];
+        String title = "";
+        boolean flag = true;
+        int h=0;
+        while(cit.charAt(h)!=')' || cit.charAt(h+1)!='.'){
+            h++;
+        }
+        
+        for (int k=h+3;k<cit.length();k++) {
+            if (cit.charAt(k)!=',' && flag) {
+                title+=cit.charAt(k);
+            }
+            else if(flag==true){
+                flag=false;
+            }
+            
+        }
+        
+        System.out.println(title);
     }
  
 }
